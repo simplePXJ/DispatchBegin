e68a2fcf7a0e583caec8ee8afb178a322f4f1eca
simplePXJ <1203469497@qq.com>
2017-05-12 14:30:53 +0800

    Initial Commit

---
+++ Dispatch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dispatch
+//
+//  Created by PXJ on 17/5/12.
+//  Copyright © 2017年 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
